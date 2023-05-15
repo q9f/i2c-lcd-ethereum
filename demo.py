e9656320461c5e691af42cd7903c940972bda1bf
Afr Schoe <58883403+q9f@users.noreply.github.com>
2023-05-15 18:02:09 +0200

    demo: all apis

--- demo.py
+++ demo.py
@@ -16,6 +16,8 @@
 
 #!/usr/bin/env python
 
+from eth.consensus import Consensus
+from eth.execution import Execution
 import i2c.lcd as lcd
 
 # use `i2cdetect` to determine device address and bus (see readme)
@@ -28,3 +30,28 @@ lcd.display_string("geth/1.11.6-stable-e", 1)
 lcd.display_string("it works!  peers: 48", 2)
 lcd.display_string("  block: #17_264_799", 3)
 lcd.display_string("b854e8e65..836af1984", 4)
+
+# List of available beacon utilities
+lodestar = Consensus("http://localhost:9596")
+print(lodestar.version)
+print(lodestar.is_connected())
+print(lodestar.is_syncing())
+print(lodestar.peer_count())
+print(lodestar.latest_block_hash())
+print(lodestar.latest_block_number())
+print(lodestar.latest_block_time())
+print(lodestar.current_epoch_number())
+print(lodestar.current_slot_number())
+print(lodestar.current_slot_time())
+
+print("---")
+
+# List of available evm utilities
+geth = Execution("http://localhost:8545")
+print(geth.version)
+print(geth.is_connected())
+print(geth.is_syncing())
+print(geth.peer_count())
+print(geth.latest_block_hash())
+print(geth.latest_block_number())
+print(geth.latest_block_time())
